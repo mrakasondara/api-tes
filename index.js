94dfd7c7a9f90d6543c7a5c41dddbf998cb5c28d
mrakasondara <rakasondara21@gmail.com>
2023-12-24 15:55:03 +0700

    tes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
-
+const port = process.env.PORT || 4000
 app.get('/get', (req,res)=>{
 	res.send('teessssssss')
+})
+
+app.listen(port, ()=>{
+    console.log(`App Listening on Port ${port}`)
 })
 
 module.exports = app
