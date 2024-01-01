2b45e3208d4e194332584789f6e9321ae745f806
mrakasondara <rakasondara21@gmail.com>
2024-01-01 15:46:37 +0700

    multer uploads

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+const multer = require('multer')
 const app = express()
 const Post = require('../Post')
+const User = require('../User')
 const port = process.env.PORT || 4000
+
+app.use(cors({credentials: true, origin: 'https://blog-titik-game.vercel.app'}))
+const uploadMiddleware = multer({dest: 'uploads/'})
+app.use(express.json())
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 
 app.get('/get', (req,res)=>{
 	res.send('teessssssss')
+})
+
+app.post('/register', async (req,res)=>{
+	mongoose.connect("mongodb+srv://rakasondara21:rakasondara21@project.ezg1faq.mongodb.net/?retryWrites=true&w=majority")
+	const {fullname,username,password} = req.body
+    try{
+        const create = await User.create({fullname,username,password:bcrypt.hashSync(password,salt)})
+        res.json(create)
+        console.log(create)
+    }catch(e){
+        console.log(e)
+        res.status(400).json(e)
+    }
 })
 
 app.get('/post', async(req,res)=>{
