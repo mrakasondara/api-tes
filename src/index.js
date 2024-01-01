68eb03bf621a07481f8dd38fb99347668e6a0e4b
mrakasondara <rakasondara21@gmail.com>
2024-01-01 15:13:54 +0700

    mongoose connect

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+const Post = require('../Post')
 const port = process.env.PORT || 4000
+
+
 app.get('/get', (req,res)=>{
 	res.send('teessssssss')
+})
+
+app.get('/post', async(req,res)=>{
+	mongoose.connect("mongodb+srv://rakasondara21:rakasondara21@project.ezg1faq.mongodb.net/?retryWrites=true&w=majority")
+	res.json(await Post.find())
 })
 
 app.listen(port, ()=>{
