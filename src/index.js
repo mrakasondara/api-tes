f595b6340e0ee315387c3b846ca70e739548fb35
mrakasondara <rakasondara21@gmail.com>
2024-01-23 20:36:02 +0700

    test cors

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ const Post = require('../Post')
 const User = require('../User')
 const port = process.env.PORT || 4000
 
-app.use(cors())
 app.use(cors({credentials: true, origin: 'https://blog-titik-game.vercel.app'}))
 const salt = bcrypt.genSaltSync(10)
 const secret = '1dhds9sdfs982snqwiqdh'
